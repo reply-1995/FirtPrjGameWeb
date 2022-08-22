dc48d0c32de1bc6c17dd6fec6b574412eb50be4d
Mokdasoo <dgstudylife@naver.com>
2022-08-22 15:52:51 +0900

    freeboardDBSave & community free list page writebutton add

--- controller/CommunityController.js
+++ controller/CommunityController.js
@@ -10,20 +10,41 @@ exports.Free_page = (req, res) => {
     }
 
     
-    // user/login에 접속하면 login.ejs 파일불러오기.
 }
 exports.Free_write = (req, res) => {
     const user = req.session.user;
-    console.log(user);
-    console.log(req.body);
-    // 로그인 했을 때
-    if (user != undefined) {
-        res.render("community_freeList", {isLogin: true, user: user});
-    } else {
-        res.render("community_freeList", {isLogin: false});
+    var today = new Date();
+
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+
+    var dateString = year + '-' + month  + '-' + day;
+
+    let object = {
+        id: user,
+        title: req.body.title,
+        content: req.body.content,
+        create_date: dateString
     }
-    //res.render("community_freeList");
+    models.Community.create( object )
+    .then((result) => {
+        console.log(result);
+        if (user != undefined) {
+            res.render("community_freeList", {isLogin: true, user: user});
+        } else {
+            res.render("community_freeList", {isLogin: false});
+        }
+    });
+
+
+    
 }
 exports.Free_writeview = (req, res) => {
-    res.render("community_freewrite");
+    const user = req.session.user;
+    if (user != undefined) {
+        res.render("community_freeWrite");
+    } else {
+        alert("로그인을 해주세요");
+    }
 }
