a6c16fe3f9d74e8eeafdfee984062bd2049d5cd7
Mokdasoo <dgstudylife@naver.com>
2022-08-22 21:28:29 +0900

    selectOne

--- routes/communityRouter.js
+++ routes/communityRouter.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get("/free", community.Free_page); //자유게시판 눌렀을때
 router.get("/free/write", community.Free_writeview); //게시판에서 글쓰기 -> 글쓰기 페이지로 이동할 때
 router.post("/free", community.Free_write)// 글쓰기 페이지에서 글작성 눌렀을 때
+router.get("/free/read", community.Free_read);
 
 module.exports = router;
