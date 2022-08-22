1094eff41bf1829c1517d06fd22c5ea3d0b6e5f2
Mokdasoo <dgstudylife@naver.com>
2022-08-22 22:33:13 +0900

    freeBoard click plus & delete

--- routes/communityRouter.js
+++ routes/communityRouter.js
@@ -7,5 +7,6 @@ router.get("/free", community.Free_page); //자유게시판 눌렀을때
 router.get("/free/write", community.Free_writeview); //게시판에서 글쓰기 -> 글쓰기 페이지로 이동할 때
 router.post("/free", community.Free_write)// 글쓰기 페이지에서 글작성 눌렀을 때
 router.get("/free/read", community.Free_read);
+router.get("/free/delete", community.Free_delete);
 
 module.exports = router;
